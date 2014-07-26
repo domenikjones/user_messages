f7915f17cb5efc6087a66423ae032df38d58e0e8
r00tl3ss <domenik.jones@gmail.com>
2014-07-26 11:24:22 +0200

    defaults urls

--- user_messages/urls.py
+++ user_messages/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url, handler404, handler500
+from django.conf.urls import patterns, include, url, handler404, handler500
 
 
 urlpatterns = patterns("user_messages.views",
