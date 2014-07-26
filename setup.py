ddf9ee963d9268a59a2bfa817f54df5e857f468c
r00tl3ss <domenik.jones@gmail.com>
2014-07-26 11:31:35 +0200

    admin 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "user-messages",
-    version = "0.1",
+    version = "0.1.1",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "a reusable private user messages application for Django",
